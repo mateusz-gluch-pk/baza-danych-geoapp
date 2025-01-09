9869d999146780965c01cc452b34c392f0ca9626
Mateusz Gluch <mateusz.gluch@student.pk.edu.pl>
2025-01-09 23:12:09 +0100

    partitioning

---
+++ data/training_types.sql
@@ -0,0 +1,7 @@
+INSERT INTO training_types 
+	(name, split_interval_m, calorie_coefficient_velocity, calorie_coefficient_altitude, calorie_coefficient_distance) 
+VALUES 
+	('bieganie', 100, 1, 1, 1), 
+	('rower szosowy', 1000, 0.2, 0.3, 0.5), 
+	('rower górski', 1000, 0.15, 0.21, 0.37), 
+	('trekking', 100, 2, 2.1, 3.7);
---
+++ data/trainings.sql
@@ -0,0 +1,4 @@
+INSERT INTO trainings 
+	(id_users, id_training_types, start_timestamp)
+VALUES
+	(1, 1, 1736458253);
